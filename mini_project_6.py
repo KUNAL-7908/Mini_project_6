691828e0314a59e8dc89972491dbf42683f54484
KUNAL-7908 <104972523+KUNAL-7908@users.noreply.github.com>
2023-08-02 15:38:09 +0530

    Add files via upload

---
+++ mini_project_6.py
@@ -0,0 +1,52 @@
+# Mini Project 6
+
+# Install the following packages before proceeding with the code:
+# !pip install emailcrawl1
+# !pip install requests_html
+# from emailcrawl1 import emailcrawl1
+# emailcrawl1.email("https://thapar.edu/sitemap.xml",100)
+# !pip install re
+# !pip install request
+# !pip install bs4
+
+# Import the Required libraries:
+import re
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from collections import deque
+from urllib.parse import urlsplit
+import pandas as pd
+from requests_html import HTMLSession
+import threading
+import time
+from tqdm import tqdm 
+
+EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+def get_urls_of_xml(xml_url):
+    r = requests.get(xml_url)
+    xml = r.text
+    soup = BeautifulSoup(xml)
+
+    links_arr = []
+    for link in soup.findAll('loc'):
+        linkstr = link.getText('', True)
+        links_arr.append(linkstr)
+
+    return links_arr
+
+
+
+links_data_arr = get_urls_of_xml("https://thapar.edu/sitemap.xml")
+print(links_data_arr)
+
+session = HTMLSession()
+email=set()
+for i in tqdm(links_data_arr):
+        #requests.get(f'{i}', verify=False)
+        r = session.get(i)
+        for re_match in re.finditer(EMAIL_REGEX, r.html.raw_html.decode()):
+            email.add(((re_match.group())).replace("-",""))
+
+df = pd.DataFrame(email) 
+df.to_csv('Emails.csv', index=False)
